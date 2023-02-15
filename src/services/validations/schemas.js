e06687125a8974aed8fc5f5d13bf8d177f62a00c
joao <joaopaz79@outlook.com>
2023-02-15 13:07:05 -0300

    Finaliza requisito 8

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -1,20 +1,20 @@
-// const Joi = require('joi');
+const Joi = require('joi');
 
-// const schemaName = Joi.string().min(5).required()
-//   .messages({
-//     'any.required': '"name" is required',
-//     'string.min': '"name" length must be at least 5 characters long',
-//   });
+const schemaName = Joi.string().min(5).required()
+  .messages({
+    'any.required': '"name" is required',
+    'string.min': '"name" length must be at least 5 characters long',
+  });
 
-// const schemaSale = Joi.object().keys({
-//   productId: Joi.number().positive().integer().required(),
-//   quantity: Joi.number().positive().integer().required(),
-// }).messages({
-//   'any.required': '"{#key}" is required',
-//   'number.positive': '"{#key}" must be greater than or equal to 1',
-// });
+const schemaSale = Joi.object().keys({
+  productId: Joi.number().positive().integer().required(),
+  quantity: Joi.number().positive().integer().required(),
+}).messages({
+  'any.required': '"{#key}" is required',
+  'number.positive': '"{#key}" must be greater than or equal to 1',
+});
 
-// module.exports = {
-//   schemaName,
-//   schemaSale,
-// };
+module.exports = {
+  schemaName,
+  schemaSale,
+};
