db5042478f2cbfeb6151d406d4f916757ecfa869
joao <joaopaz79@outlook.com>
2023-02-15 13:12:54 -0300

    Testa se o req 6 passa

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -4,6 +4,7 @@ const schemaName = Joi.string().min(5).required()
   .messages({
     'any.required': '"name" is required',
     'string.min': '"name" length must be at least 5 characters long',
+    
   });
 
 const schemaSale = Joi.object().keys({
