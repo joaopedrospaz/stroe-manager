ecf5a786596684b74f392826dc9ed01525fc60cb
joao <joaopaz79@outlook.com>
2023-03-29 13:58:41 -0300

    Adiciona penedencias

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -3,13 +3,12 @@ const productsModel = require('../models/productsModel');
 
 const getAllProducts = async () => {
   const allProducts = await productsModel.getAll();
-
   return { type: null, message: allProducts };
 };
 
 const getProductById = async (productId) => {
   const product = await productsModel.getById(productId);
-
+  
   if (!product) return { type: 'NOT_FOUND', message: 'Product not found' };
   
   return { type: null, message: product };
